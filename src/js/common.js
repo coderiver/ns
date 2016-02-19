01472488df14b5ae48222563f8e4518330db6e3d
etna-scorpio <etna.scorpio@gmail.com>
2016-02-19 18:17:25 +0200

    upd

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
+	var a = new Date(); // Now
+	var b = new Date(2016, 3, 20, 0, 0, 0, 0); // end date
+	var seconds = Math.round((b - a) / 1000); // seconds
 
-	// go js
+	clock = $('.js-clock').FlipClock({
+		clockFace: 'DailyCounter',
+		language: 'ru'
+	});
+
+	clock.setTime(seconds);
+	clock.setCountdown(true);
+	clock.start();
 
 });
